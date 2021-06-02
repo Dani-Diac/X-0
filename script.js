ba4bc711fe8643cfb10514b24583117af596e4f0
Dani-Diac <77856443+Dani-Diac@users.noreply.github.com>
2021-06-02 08:13:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,123 +8,98 @@ function game(id) {
     document.getElementById(id).innerHTML = player;
     var disableBox = document.getElementById(id);
     disableBox.onclick = "";
-    checkFirstLine();
+    checkLines();
   }
 }
 
-function checkFirstLine() {
+function checkLines() {
+  let firstLineX = 3, secondLineX = 3, thirdLineX = 3;
+  let firstLine0 = 3, secondLine0 = 3, thirdLine0 = 3;
   const firstLine = document.querySelectorAll("#square_1, #square_2, #square_3");
-  if (firstLine[0].innerHTML == "✘" && firstLine[1].innerHTML ==  "✘" && firstLine[2].innerHTML == "✘") {
-    winner = true;
-    Message();
-    return;
-  }
-  if (firstLine[0].innerHTML == "0" && firstLine[1].innerHTML ==  "0" && firstLine[2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkSecondLine();
-}
-
-function checkSecondLine() {
   const secondLine = document.querySelectorAll("#square_4, #square_5, #square_6");
-  if (secondLine[0].innerHTML == "✘" && secondLine[1].innerHTML == "✘" && secondLine[2].innerHTML == "✘") {
-    winner = true;
-    Message();
-    return;
-  }
-  if (secondLine[0].innerHTML == "0" && secondLine[1].innerHTML == "0" && secondLine[2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkThirdLine();
-}
-
-function checkThirdLine() {
   const thirdLine = document.querySelectorAll("#square_7, #square_8, #square_9");
-  if (thirdLine[0].innerHTML == "✘" && thirdLine[1].innerHTML == "✘" && thirdLine[2].innerHTML == "✘") {
+  for (var i = 0; i < 3; i++) {
+    if (firstLine[i].innerHTML == "✘") {
+      --firstLineX;
+    }
+    if (secondLine[i].innerHTML == "✘") {
+      --secondLineX;
+    }
+    if (thirdLine[i].innerHTML == "✘") {
+      --thirdLineX;
+    }
+    if (firstLine[i].innerHTML == "0") {
+      --firstLine0;
+    }
+    if (secondLine[i].innerHTML == "0") {
+      --secondLine0;
+    }
+    if (thirdLine[i].innerHTML == "0") {
+      --thirdLine0;
+    }
+  }
+  if (firstLineX == 0 || secondLineX == 0 || thirdLineX == 0 || firstLine0 == 0 || secondLine0 == 0 || thirdLine0 == 0) {
     winner = true;
     Message();
     return;
   }
-  if (thirdLine[0].innerHTML == "0" && thirdLine[1].innerHTML == "0" && thirdLine[2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkFirstRow();
+  checkRows();
 }
 
-function checkFirstRow() {
+function checkRows() {
+  let firstRowX = 3, secondRowX = 3, thirdRowX = 3;
+  let firstRow0 = 3, secondRow0 = 3, thirdRow0 = 3;
   const firstRow = document.querySelectorAll("#square_1, #square_4, #square_7");
-  if (firstRow[0].innerHTML == "✘" && firstRow[1].innerHTML == "✘" && firstRow[2].innerHTML == "✘") {
-    winner = true;
-    Message();
-    return;
-  }
-  if (firstRow[0].innerHTML == "0" && firstRow[1].innerHTML == "0" && firstRow[2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkSecondRow();
-}
-
-function checkSecondRow() {
   const secondRow = document.querySelectorAll("#square_2, #square_5, #square_8");
-  if (secondRow[0].innerHTML == "✘" && secondRow[1].innerHTML == "✘" && secondRow[2].innerHTML == "✘") {
-    winner = true;
-    Message();
-    return;
-  }
-  if (secondRow[0].innerHTML == "0" && secondRow[1].innerHTML == "0" && secondRow[2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkThirdRow();
-}
-
-function checkThirdRow() {
   const thirdRow = document.querySelectorAll("#square_3, #square_6, #square_9");
-  if (thirdRow[0].innerHTML == "✘" && thirdRow[1].innerHTML == "✘" && thirdRow [2].innerHTML == "✘") {
+  for (var i = 0; i < 3; i++) {
+    if (firstRow[i].innerHTML == "✘") {
+      --firstRowX;
+    }
+    if (secondRow[i].innerHTML == "✘") {
+      --secondRowX;
+    }
+    if (thirdRow[i].innerHTML == "✘") {
+      --thirdRowX;
+    }
+    if (firstRow[i].innerHTML == "0") {
+      --firstRow0;
+    }
+    if (secondRow[i].innerHTML == "0") {
+      --secondRow0;
+    }
+    if (thirdRow[i].innerHTML == "0") {
+      --thirdRow0;
+    }
+  }
+  if (firstRowX == 0 || secondRowX == 0 || thirdRowX == 0 || firstRow0 == 0 || secondRow0 == 0 || thirdRow0 == 0) {
     winner = true;
     Message();
     return;
   }
-  if (thirdRow[0].innerHTML == "0" && thirdRow[1].innerHTML == "0" && thirdRow [2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkFirstDiagonal();
+  checkDiagonals();
 }
 
-function checkFirstDiagonal() {
+function checkDiagonals() {
+  let firstDiagonalX = 3, secondDiagonalX = 3;
+  let firstDiagonal0 = 3, secondDiagonal0 = 3;
   const firstDiagonal = document.querySelectorAll("#square_1, #square_5, #square_9");
-  if (firstDiagonal[0].innerHTML == "✘" && firstDiagonal[1].innerHTML == "✘" && firstDiagonal[2].innerHTML == "✘") {
-    winner = true;
-    Message();
-    return;
-  }
-  if (firstDiagonal[0].innerHTML == "0" && firstDiagonal[1].innerHTML == "0" && firstDiagonal[2].innerHTML == "0") {
-    winner = true;
-    Message();
-    return;
-  }
-  checkSecondDiagonal();
-}
-
-function checkSecondDiagonal() {
   const secondDiagonal = document.querySelectorAll("#square_3, #square_5, #square_7");
-  if (secondDiagonal[0].innerHTML == "✘" && secondDiagonal[1].innerHTML == "✘" && secondDiagonal[2].innerHTML == "✘") {
-    winner = true;
-    Message();
-    return;
+  for (var i = 0; i < 3; i++) {
+    if (firstDiagonal[i].innerHTML == "✘") {
+      --firstDiagonalX;
+    }
+    if (secondDiagonal[i].innerHTML == "✘") {
+      --secondDiagonalX;
+    }
+    if (firstDiagonal[i].innerHTML == "0") {
+      --firstDiagonal0;
+    }
+    if (secondDiagonal[i].innerHTML == "0") {
+      --secondDiagonal0;
+    }
   }
-  if (secondDiagonal[0].innerHTML == "0" && secondDiagonal[1].innerHTML == "0" && secondDiagonal[2].innerHTML == "0") {
+  if (firstDiagonalX == 0 || secondDiagonalX == 0 || firstDiagonal0 == 0 || secondDiagonal0 == 0) {
     winner = true;
     Message();
     return;
