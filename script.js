fe9b95daa4898d789eea23eaa45e0c1067264f4f
Dani-Diac <77856443+Dani-Diac@users.noreply.github.com>
2021-06-16 17:08:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,103 +10,56 @@ function game(id) {
     disableBox.onclick = "";
     checkLines();
   }
+
 }
 
 function checkLines() {
-  let firstLineX = 3, secondLineX = 3, thirdLineX = 3;
-  let firstLine0 = 3, secondLine0 = 3, thirdLine0 = 3;
-  const firstLine = document.querySelectorAll("#square_1, #square_2, #square_3");
-  const secondLine = document.querySelectorAll("#square_4, #square_5, #square_6");
-  const thirdLine = document.querySelectorAll("#square_7, #square_8, #square_9");
-  for (var i = 0; i < 3; i++) {
-    if (firstLine[i].innerHTML == "✘") {
-      --firstLineX;
+  for (var i = 1; i <= 7; i+= 3) {
+    if (document.getElementById(i).innerHTML == "✘" && document.getElementById(i + 1).innerHTML == "✘" && document.getElementById(i + 2).innerHTML == "✘") {
+      winner = true;
+      Message();
+      return;
     }
-    if (secondLine[i].innerHTML == "✘") {
-      --secondLineX;
+    if (document.getElementById(i).innerHTML == "0" && document.getElementById(i + 1).innerHTML == "0" && document.getElementById(i + 2).innerHTML == "0") {
+      winner = true;
+      Message();
+      return;
     }
-    if (thirdLine[i].innerHTML == "✘") {
-      --thirdLineX;
-    }
-    if (firstLine[i].innerHTML == "0") {
-      --firstLine0;
-    }
-    if (secondLine[i].innerHTML == "0") {
-      --secondLine0;
-    }
-    if (thirdLine[i].innerHTML == "0") {
-      --thirdLine0;
-    }
-  }
-  if (firstLineX == 0 || secondLineX == 0 || thirdLineX == 0 || firstLine0 == 0 || secondLine0 == 0 || thirdLine0 == 0) {
-    winner = true;
-    Message();
-    return;
   }
   checkRows();
 }
 
 function checkRows() {
-  let firstRowX = 3, secondRowX = 3, thirdRowX = 3;
-  let firstRow0 = 3, secondRow0 = 3, thirdRow0 = 3;
-  const firstRow = document.querySelectorAll("#square_1, #square_4, #square_7");
-  const secondRow = document.querySelectorAll("#square_2, #square_5, #square_8");
-  const thirdRow = document.querySelectorAll("#square_3, #square_6, #square_9");
-  for (var i = 0; i < 3; i++) {
-    if (firstRow[i].innerHTML == "✘") {
-      --firstRowX;
+  for (var i = 1; i <= 3; ++i) {
+    if (document.getElementById(i).innerHTML == "✘" && document.getElementById(i + 3).innerHTML == "✘" && document.getElementById(i + 6).innerHTML == "✘") {
+      winner = true;
+      Message();
+      return;
     }
-    if (secondRow[i].innerHTML == "✘") {
-      --secondRowX;
+    if (document.getElementById(i).innerHTML == "0" && document.getElementById(i + 3).innerHTML == "0" && document.getElementById(i + 6).innerHTML == "0") {
+      winner = true;
+      Message();
+      return;
     }
-    if (thirdRow[i].innerHTML == "✘") {
-      --thirdRowX;
-    }
-    if (firstRow[i].innerHTML == "0") {
-      --firstRow0;
-    }
-    if (secondRow[i].innerHTML == "0") {
-      --secondRow0;
-    }
-    if (thirdRow[i].innerHTML == "0") {
-      --thirdRow0;
-    }
-  }
-  if (firstRowX == 0 || secondRowX == 0 || thirdRowX == 0 || firstRow0 == 0 || secondRow0 == 0 || thirdRow0 == 0) {
-    winner = true;
-    Message();
-    return;
   }
   checkDiagonals();
 }
 
 function checkDiagonals() {
-  let firstDiagonalX = 3, secondDiagonalX = 3;
-  let firstDiagonal0 = 3, secondDiagonal0 = 3;
-  const firstDiagonal = document.querySelectorAll("#square_1, #square_5, #square_9");
-  const secondDiagonal = document.querySelectorAll("#square_3, #square_5, #square_7");
-  for (var i = 0; i < 3; i++) {
-    if (firstDiagonal[i].innerHTML == "✘") {
-      --firstDiagonalX;
-    }
-    if (secondDiagonal[i].innerHTML == "✘") {
-      --secondDiagonalX;
-    }
-    if (firstDiagonal[i].innerHTML == "0") {
-      --firstDiagonal0;
-    }
-    if (secondDiagonal[i].innerHTML == "0") {
-      --secondDiagonal0;
-    }
+  if (document.getElementById(1).innerHTML == "✘" && document.getElementById(5).innerHTML == "✘" && document.getElementById(9).innerHTML == "✘" ||
+      document.getElementById(3).innerHTML == "✘" && document.getElementById(5).innerHTML == "✘" && document.getElementById(7).innerHTML == "✘") {
+    winner = true;
+    Message();
+    return;
   }
-  if (firstDiagonalX == 0 || secondDiagonalX == 0 || firstDiagonal0 == 0 || secondDiagonal0 == 0) {
+  if (document.getElementById(1).innerHTML == "0" && document.getElementById(5).innerHTML == "0" && document.getElementById(9).innerHTML == "0" ||
+      document.getElementById(3).innerHTML == "0" && document.getElementById(5).innerHTML == "0" && document.getElementById(7).innerHTML == "0") {
     winner = true;
     Message();
     return;
   }
   setPlayer();
 }
-
 function setPlayer() {
   if (player == "✘") {
     player = "0";
